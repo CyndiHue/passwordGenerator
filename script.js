a1a61bf02819fab0562061b3ef68626c48c1d1be
CyndiCodes <huertascyn@gmail.com>
2023-07-25 17:30:16 -0400

    general functions layout in script.js

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [0,1,2,3,4,5,6,7,8,9]
-let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-// added from video 
+let specialCharacters = ['#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '!', '"'];
 
-let builtArray = []
-
-
-
-
+// let passwordLength = "";
+// let generatePassword = []
 
 function generatePassword(){
+  
+  if(confirm("Do you want your password to have lower case letters?")){
+  
+  }
+  
+  if(confirm("Do you want your password to have upper case letters?")){
+    
+  }
+  
+  if(confirm("Do you want your password to have numbers?")){
+    
+  }
+  
+  if(confirm("Do you want your password to have special characters?")){
+    
+  }
+  
 
-if(confirm("Do you want you password to contain lower case letter?"))
-{
-  builtArray = [...builtArray, ...lowerAlphabet];
-}
+  let password= ""; 
 
-let password = "";
 
-let passwordLength = prompt ("how long do you want the password?");
-
-for (let i = 0; i < passwordLength; i++) {
-
-  password += "x";
-
-}
+  let passwordLength = prompt("How many characters would like your password to have (must have at least 8 characters but no more than 128 characters?")
+  for (let i = 0; i < passwordLength; i++) {
+    
+    password+= "x";
+    
+  }
 
 return password;
 }
 
-// Write password to the #password input
+
+let passwordText = document.querySelector("#password")
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password")
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
