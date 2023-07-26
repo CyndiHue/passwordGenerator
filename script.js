b0745962b7843d78734ee11699a9a3cde167ef50
CyndiCodes <huertascyn@gmail.com>
2023-07-26 16:41:55 -0400

    added parameters for varible passwordLength and varible builtArray.length === 0

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [0,1,2,3,4,5,6,7,8,9]
 let specialCharacters = ['#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '!', '"'];
+// let charactersAllowed = []
 
 function generatePassword(){
   let builtArray =[]
   let randomGenerator = Math.floor(Math.random()*builtArray.length);
+
   if(confirm("Do you want your password to have lower case letters?")){
     builtArray = [...builtArray, ...lowerAlphabet]
   }
@@ -22,20 +24,30 @@ function generatePassword(){
   
   if(confirm("Do you want your password to have special characters?")){
     builtArray = [...builtArray, ...specialCharacters]
+    
   }
   
-
   let passwordLength = prompt("How many characters would like your password to have (must have at least 8 characters but no more than 128 characters?")
   let password= "";
 
-  for (let i = 0; i < passwordLength; i++) {
-    
-    password+= builtArray[Math.floor(Math.random()*builtArray.length)];
-    // need to make this random 1 value not full value
-    
+  if (builtArray.length === 0){
+    alert("please try again, you must have at least one character type");
   }
 
-return password;
+  if (passwordLength >= 8 && passwordLength <= 128 ){
+
+    for (let i = 0; i < passwordLength; i++) {
+      password+= builtArray [Math.floor(Math.random()*builtArray.length)];
+     
+    }
+    return password;
+
+  }else {
+  alert("undefined* password length must be between 8 and 128 character long. please try again")
+
+
+  } 
+
 }
 
 
